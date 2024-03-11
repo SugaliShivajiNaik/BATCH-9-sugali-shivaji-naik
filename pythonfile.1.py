be1a32755906bf7faeb49f18f4cfc04740cc0bb1
SugaliShivajiNaik <162983054+SugaliShivajiNaik@users.noreply.github.com>
2024-03-11 16:02:23 +0530

    Add files via upload

---
+++ pythonfile.1.py
@@ -0,0 +1,66 @@
+Bargained characters
+Controls and Data structures → ABC language
+Strings and its formats from C
+Regular expressions → Pearl
+Exception handling Module 3
+Magic methods from → fortron
+
+#print("hello world")
+#print("hello 1")
+
+
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+'''
+----> Commenting
+1. Commenting is used for making documentation in a python file
+2. When the line is commented, that line will not be considered as a part of the code.
+3. When the program is executed, commented line will not be in the part of execution.
+> NOTE:
+Single line commenting can be done using # at the begining of the line
+Eg:
+#print("hello 1")
+I Multiple line commenting can be done using code . The lines between the quotes will be commented.
+Eg:
+   
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+        
+print("hello world")
+
+print("hello world")
+
+'''
+import this
+#import is a keywordwhich is used to communicated between the python file
+
+flavours of pythaon
+
+> Cpython-Written in c, most common implementation of python.
+> jython - java implementation of the python programming languages.
+>ironpython - implemention of the python programming languages
+>Brython - Browser python, runs in browsers.
+> rubypython - Bridegebetween python and ruby interpreters.
+> Micropython - Runs in Micro controllers.
+
+
+
+
+
+
+
+
+
+
+
+
