80d754e382d5bf52ea9c93c19cb3a2f0cd3c48bc
SugaliShivajiNaik <162983054+SugaliShivajiNaik@users.noreply.github.com>
2024-03-21 15:52:34 +0530

    Add files via upload

--- pythonfile.10.py
+++ pythonfile.10.py
@@ -1,6 +1,21 @@
-
+# ! -----> tasks 
 # write the code for the below tasks using function
 # 1.)d1 = {"shirt": 1000, "pant":1500, "shoes": "900", "handkey": 30}
+
+d1 = {"shirt": 1000, "part": 1500, "shoes": 900, "handkey": 30}
+for val in d1:
+    if d1[val] == min(d1.values()):
+        print(val)
+for val in d1:
+    of d1[val] == max(d1.values()):
+        print(val)
+for val in d1:
+    if val.startswitch('s') or val.startswith('s'):
+        print(val)
+
+
+
+
 # a.) find the min ans max priced product
 # b.) find the product starts with 's' and 's'
 
@@ -120,8 +135,156 @@ obj.add(1,2,3)
 
 
 # ! --------> Abstraction
+the procss of hiding the implimentation details is abstraction
+
+# ? Eg:1
+from abc import abc,abstractmethod
+class shaps(abc):
+    @abstractmethod
+    def sides(self):
+        print('all shapes have sides except circle')
+        
+class triangle:
+    def triangle_sides(self):
+        print("3 sides")
 
 
+class square(shapes):
+    def square(self):
+        print("4 sides")
+
+tr = triangle()
+tr.sides()
+
+
+
+def decor(func):
+    def inner():
+        str1 = func()
+        return str1.upper()
+    return inner()
+
+@decor
+def greet():
+    return 'good morning'
+print(greet)
+
+class triangle(shapes):
+    def traingle_sides(self):
+        print("3 sides")
+
+    def name(self):
+        print("Iam traingle")
+
+   def sides (self):
+       pass
+
+class square(shapes):
+    def square(self):
+        print("4 sides")
+        
+tr triangle()
+tr.traingle_sides()
+tr.name()
+
+# ! Rules to define abstract class1
+1.) abract class cannot be instantiated
+2.) from abc import abc, abstractmethod
+3.) subclass the normal class with ABC
+4.) convert the normal method inside the abstract class to abstract method
+5.) all the child classes have to be subclassed with abstract class
+6.) the abstract method should be present in the
+child classes
+
+# ! Eg:2
+# * super() --> used to access the parent class method from class method
+from abc import ABC, abstractmethod
+class c1(ABC):
+    @abstractmthod
+    def m1(self):
+        print("this is abstract class")
+
+class c2(c1):
+    def m2(self):
+        super().m1()
+        print("iam child 1")
+
+    def m1(self):
+        pass
+
+class1 = c1()
+class2.m2()
+
+class2 = c2()
+class.m2()
+
+# *Eg:3
+from abc import ABC, abstractmethod
+class password:
+    @abstractmethod
+    def pwd(self):
+    pswd = "sidd1994$"
+    return pswd
+
+class login(password):
+    def valindate(self, name, passwrd):
+        if super().pwd() ==passwrd
+        print(name, password)
+        print("Welcome", name,'!!')
+    else:
+        print("please check the password")
+
+    def pwd(self):
+        pass
+
+login = login()
+name = input("Enter the name: ")
+pwd = input("Enter the password: ")
+login.validate(name, pwd)
+
+# ! Encapsulation
+# * ---> Eg:1
+class car:
+    __name = "BMW" # private variable
+    print(__name)
+
+c1 = car()
+print(c1.name) # error
+c1.name = "Audi"
+print(c1.name) # error
+
+# * --->Eg:2
+# ? Accessing private date outside the class
+class c1:
+    __phone = '7656567687'
+
+    def display(self):
+        print(self.__phone)
+c = c1()
+c.display()
+
+# * ----> Eg:3
+# ? declare private method
+class class1:
+    def __m1(self): # Private method
+        print("Iam private method")
+
+    def __init__(self):
+        self.__m1()
+c = class1()
+c.__m1() # error
+
+# ? nested class
+class class1:
+    class class2:
+        name = "sidhu"
+
+        def display(self):
+            print(self.name)
+    obj1 = class2()
+
+obj = class1()
+obj.obj1.display()
 
 
 
